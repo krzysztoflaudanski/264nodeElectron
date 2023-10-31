8f711d70481a45b2c11aed77e27e433f1d300e33
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-10-31 20:18:56 +0100

    fix images

--- app/script.js
+++ app/script.js
@@ -58,8 +58,8 @@ const App = () => {
                 <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
                 <p>This app will help you track your time and inform you when it's time to rest.</p>
             </div>}
-            {(status === 'work') && <img src="./images/work.png" />}
-            {(status === 'rest') && <img src="./images/rest.png" />}
+            {(status === 'work') && <img src="./images/Work.png" />}
+            {(status === 'rest') && <img src="./images/Rest.png" />}
             {(status !== 'off') && <div className="timer">
                 {formatTime(time)}
             </div>}
